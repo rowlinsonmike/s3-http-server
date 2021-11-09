054c81ebe9f58341f07bacb2734057fbc2472563
Michael Rowlinson <michael.rowlinson@westrock.com>
2021-11-09 16:24:21 -0500

    :necktie: Serve Websites
    can now serve an index.html file and assets from s3 bucket with -w switch

--- bin/s3-http-server.js
+++ bin/s3-http-server.js
@@ -7,13 +7,22 @@ process.title = "s3-http-server";
 if (argv.h || argv.help || !argv._.length) {
   console.log(`
   usage: s3-http-server [bucket] [options]
+  -w --web           Serve a website
   -h --help          Print this list and exit.
   `);
   process.exit();
 }
 const bucket = argv._[0];
 const port = 8080;
-const httpServer = require("../lib/s3-http-server").createServer({ bucket });
+
+let httpServer = null;
+if (argv.w || argv.web) {
+  httpServer = require("../lib/s3-http-server").createWebServer({
+    bucket,
+  });
+} else {
+  httpServer = require("../lib/s3-http-server").createServer({ bucket });
+}
 
 httpServer.listen(port, function () {
   console.log(`
